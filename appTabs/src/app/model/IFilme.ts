10e0f3df43b78c6157176118e80207b3a64d6992
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-05-28 12:29:31 -0300

    Arquivo completo IFilme.ts

--- appTabs/src/app/model/IFilme.ts
+++ appTabs/src/app/model/IFilme.ts
@@ -1,1 +1,11 @@
 /* eslint-disable eol-last */
+export interface IFilme{
+    nome: string;
+    lancamento: string;
+    duracao: string;
+    classificacao: number;
+    cartaz: string;
+    generos: string[];
+    pagina?: string; // *?* -> Campo não obrigatório
+    favorito: boolean;
+}
