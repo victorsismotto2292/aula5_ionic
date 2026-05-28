e203374fe08796a130ba63ea40b45f8fd3939539
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-05-28 14:57:47 -0300

    Página filmeDetalhe adicionada

---
+++ appTabs/src/app/filme-detalhe/filme-detalhe.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filme-detalhe',
+  templateUrl: './filme-detalhe.page.html',
+  styleUrls: ['./filme-detalhe.page.scss'],
+})
+export class FilmeDetalhePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
