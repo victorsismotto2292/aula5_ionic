43b267a08fd16007240afa914dda1f4e481e23bf
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-05-28 15:19:21 -0300

    Código do arquivo filme-detalhe.page.ts

--- appTabs/src/app/filme-detalhe/filme-detalhe.page.ts
+++ appTabs/src/app/filme-detalhe/filme-detalhe.page.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+/* eslint-disable @typescript-eslint/dot-notation */
+import { IFilme } from './../model/IFilme';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-filme-detalhe',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filme-detalhe.page.scss'],
 })
 export class FilmeDetalhePage implements OnInit {
+  filme: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const getNav = this.router.getCurrentNavigation();
+      if(getNav?.extras.state){
+        this.filme = getNav.extras.state['paramFilme'];
+      }
+    })
   }
 
 }
