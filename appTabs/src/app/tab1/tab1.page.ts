9855d39df119b9919a3ca30ee0e53f8ba8c99efc
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-05-28 12:35:00 -0300

    Criando o array de filmes

--- appTabs/src/app/tab1/tab1.page.ts
+++ appTabs/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFilme } from '../model/IFilme';
 
 @Component({
   selector: 'app-tab1',
@@ -9,5 +10,27 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+  listaFilmes: IFilme[] = [
+  {
+    nome: 'Interestelar',
+    lancamento: '2014-11-06',
+    duracao: '2h 49m',
+    classificacao: 8.7,
+    cartaz: 'assets/cartazes/interestelar.jpg',
+    generos: ['Ficção Científica', 'Drama', 'Aventura'],
+    pagina: '/interstellar',
+    favorito: true
+  },
+  {
+    nome: 'Matrix',
+    lancamento: '1999-05-21',
+    duracao: '2h 16m',
+    classificacao: 8.7,
+    cartaz: 'assets/cartazes/matrix.jpg',
+    generos: ['Ação', 'Ficção Científica'],
+    pagina: '/matrix',
+    favorito: false
+  }
+];
 
 }
