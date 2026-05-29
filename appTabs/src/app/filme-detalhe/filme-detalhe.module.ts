1518f7322b701e8bea464780045aff0a6f5946e9
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-05-28 21:40:16 -0300

    Arrumando tudo
    Tudo arrumando (sem adicionar método de favoritar, apenas parte dos filmes funcionando)

--- appTabs/src/app/filme-detalhe/filme-detalhe.module.ts
+++ appTabs/src/app/filme-detalhe/filme-detalhe.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { FilmeDetalhePageRoutingModule } from './filme-detalhe-routing.module';
-
 import { FilmeDetalhePage } from './filme-detalhe.page';
-
-import { Component } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -19,12 +14,4 @@ import { Component } from '@angular/core';
   ],
   declarations: [FilmeDetalhePage]
 })
-
-@Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
-  standalone: false,
-})
-
 export class FilmeDetalhePageModule {}
--- appTabs/src/app/filme-detalhe/filme-detalhe.page.ts
+++ appTabs/src/app/filme-detalhe/filme-detalhe.page.ts
@@ -1,26 +1,21 @@
-import { Router } from '@angular/router';
-/* eslint-disable @typescript-eslint/dot-notation */
-import { IFilme } from './../model/IFilme';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-filme-detalhe',
   templateUrl: './filme-detalhe.page.html',
   styleUrls: ['./filme-detalhe.page.scss'],
+  standalone: false,
 })
 export class FilmeDetalhePage implements OnInit {
   filme: any;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
-
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
-      const getNav = this.router.getCurrentNavigation();
-      if(getNav?.extras.state){
-        this.filme = getNav.extras.state['paramFilme'];
-      }
-    })
+  constructor(private route: ActivatedRoute, private router: Router) {
+    const getNav = this.router.getCurrentNavigation();
+    if (getNav?.extras.state) {
+      this.filme = getNav.extras.state['paramFilme'];
+    }
   }
 
+  ngOnInit() {}
 }
